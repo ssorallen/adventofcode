631dc30faa6c8d58ec255f1de3501f720cdfa68f
Ross Allen <rossgit@gmail.com>
2024-12-08 16:08:35 -0500

    Add solution for Day4, part 2 ⭐️
    The checking algorithm is exact, assumes a string of length 3, and would
    not be immediately usable for other variations. It checks every viable
    point in the puzzle 1 time and uses no extra memory when checking.

--- 2024/day4/day4.go
+++ 2024/day4/day4.go
@@ -7,25 +7,9 @@ import (
 	"strings"
 )
 
-type Direction struct {
-	x int
-	y int
-}
-
 type Point struct {
 	x int
 	y int
-}
-
-var directions = []Direction{
-	{x: 1, y: 0},
-	{x: 1, y: 1},
-	{x: 0, y: 1},
-	{x: -1, y: 1},
-	{x: -1, y: 0},
-	{x: -1, y: -1},
-	{x: 0, y: -1},
-	{x: 1, y: -1},
 }
 
 func get_input() [][]string {
@@ -43,42 +27,27 @@ func get_input() [][]string {
 }
 
 func count_pattern_point(input [][]string, point Point, pattern []string) int {
-	// First character does not match, nothing left to check
-	if input[point.y][point.x] != pattern[0] {
+	// First character does not match center; nothing left to check
+	if input[point.y][point.x] != pattern[1] {
 		return 0
 	}
 
-	count := 0
-	for _, direction := range directions {
-		matches := true
-		for j, char := range pattern {
-			input_x := point.x + (direction.x * j)
-			input_y := point.y + (direction.y * j)
-			if input_x < 0 || input_y < 0 {
-				matches = false
-				break
-			}
-			if input_y > (len(input) - 1) {
-				matches = false
-				break
-			}
-			if input_x > (len(input[input_y]) - 1) {
-				matches = false
-				break
-			}
-			// log.Printf("input char[%v, %v]: %v, compare char: %v", input_y, input_x, input[input_y][input_x], char)
-			if input[input_y][input_x] != char {
-				matches = false
-				break
-			}
-		}
-
-		if matches {
-			// log.Printf("direction: %v; point: %v", direction, point)
-			count += 1
-		}
+	if point.y < 1 || point.y > len(input)-2 {
+		return 0
 	}
-	return count
+
+	if point.x < 1 || point.x > len(input[point.y])-2 {
+		return 0
+	}
+
+	if ((input[point.y-1][point.x-1] == pattern[0] && input[point.y+1][point.x+1] == pattern[2]) ||
+		(input[point.y-1][point.x-1] == pattern[2] && input[point.y+1][point.x+1] == pattern[0])) &&
+		((input[point.y+1][point.x-1] == pattern[0] && input[point.y-1][point.x+1] == pattern[2]) ||
+			(input[point.y+1][point.x-1] == pattern[2] && input[point.y-1][point.x+1] == pattern[0])) {
+		return 1
+	}
+
+	return 0
 }
 
 func count_pattern(input [][]string, pattern []string) int {
@@ -93,6 +62,6 @@ func count_pattern(input [][]string, pattern []string) int {
 
 func main() {
 	input := get_input()
-	count := count_pattern(input, []string{"X", "M", "A", "S"})
+	count := count_pattern(input, []string{"M", "A", "S"})
 	log.Printf("%v", count)
 }
